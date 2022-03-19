80f43adab6381799000fb0d773f25ec856cfc708
whes1015 <44525760+whes1015@users.noreply.github.com>
2022-03-19 16:25:57 +0800

    Add files via upload

--- lineBot.js
+++ lineBot.js
@@ -15,43 +15,58 @@ const privateKey = fs.readFileSync(ExpTech.sslKey, 'ascii')
 const certificate = fs.readFileSync(ExpTech.cert, 'ascii')
 const ca = fs.readFileSync(ExpTech.ca, 'ascii')
 //#endregion
+//#region 同步讀取資料
+let User = JSON.parse(fs.readFileSync('./Json/Line/User.json').toString())
+let Cache = JSON.parse(fs.readFileSync('./Json/Line/Cache.json').toString())
+//#endregion
+
+//#region 變數
+let ver = "22w12-pre2"
+//#endregion
 
 //#region 初始化
 const credentials = { ca: ca, key: privateKey, cert: certificate }
 const httpsServer = https.createServer(credentials, app)
-const bot = linebot({
+const client = linebot({
     channelId: ExpTech.channelId,
     channelSecret: ExpTech.channelSecret,
     channelAccessToken: ExpTech.channelAccessToken
 })
-const linebotParser = bot.parser()
+const linebotParser = client.parser()
 app.post('/webhook', linebotParser)
 //#endregion
 
-//#region 同步讀取資料
-let User = JSON.parse(fs.readFileSync('./Line/Json/User.json').toString())
-//#endregion
-
 //#region 好友
-bot.on('follow', function (event) {
-    //#region 儲存用戶ID 廣播用
-    if (User["AllUser"].indexOf(event.source.userId) == -1) {
-        User["AllUser"].push(event.source.userId)
-        //同步儲存 且 格式化 Json
-        fs.writeFileSync('./Line/Json/User.json', JSON.stringify(User, null, "\t"))
-    }
-    //#endregion 
-});
+client.on('follow', function (event) {
+    event.reply("")
+})
 //#endregion
 
 setInterval(async () => {
-
+    var time = (new Date().getMonth() + 1).toString() + new Date().getDate().toString()
+    if (Cache["TimeStamp"] != time && new Date().getHours() >= 6) {
+        Cache["TimeStamp"] = time
+        fs.writeFileSync('./Json/Line/Cache.json', JSON.stringify(Cache, null, "\t"))
+        for (let index = 0; index < User["AllUser"].length; index++) {
+            (User["AllUser"][index], "早安")
+        }
+    }
 }, 30000)
 
 //#region 訊息處理區塊
-bot.on('message', async function (event) {
-    //console.log(event)
-    if (event.message.text=="天氣") {
+client.on('message', async function (event) {
+    if (event.message.text == "即時氣象數據") {
+        event.reply("CAAWMS\n複合式全天候自動氣象監測系統\n(Composite All-weather Automatic Weather Monitoring System)\n\n之後學校會架設監測站\n敬請期待")
+    } else if (event.message.text == "小時精準預報") {
+        let data = {
+            "APIkey": "a5ef9cb2cf9b0c86b6ba71d0fc39e329",
+            "Function": "data",
+            "Type": "dwhs-weather-accuweather",
+            "FormatVersion": 1
+        }
+        let res = await API.main(ExpTech, data)
+        event.reply(res.data["response"])
+    } else if (event.message.text == "氣象局預報") {
         let data = {
             "APIkey": "a5ef9cb2cf9b0c86b6ba71d0fc39e329",
             "Function": "data",
@@ -59,10 +74,60 @@ bot.on('message', async function (event) {
             "FormatVersion": 1
         }
         let res = await API.main(ExpTech, data)
-        console.log(res.data)
-        event.reply(res.data["response"]["title"] + "\n\n[最新衛星雲圖]\n" + res.data["response"]["picture"])
+        event.reply(res.data["response"]["title"].replace("溫度", "\n溫度").replace("降雨機率", "\n降雨機率").replace("%", "%\n"))
+    } else if (event.message.text == "最新衛星雲圖") {
+        let data = {
+            "APIkey": "a5ef9cb2cf9b0c86b6ba71d0fc39e329",
+            "Function": "data",
+            "Type": "dwhs-weather-cwb",
+            "FormatVersion": 1
+        }
+        let res = await API.main(ExpTech, data)
+        event.reply({
+            type: 'image',
+            originalContentUrl: res.data["response"]["picture"],
+            previewImageUrl: res.data["response"]["picture"]
+        })
+        //client.push(event.source.userId,res.data["response"]["picture"])
+    } else if (event.message.text == "最新地震") {
+        let data = {
+            "APIkey": "a5ef9cb2cf9b0c86b6ba71d0fc39e329",
+            "Function": "data",
+            "Type": "earthquake",
+            "FormatVersion": 1
+        }
+        let res = await API.main(ExpTech, data)
+        let response = res.data["response"]["earthquakeReport"]
+        event.reply("類型: " + response[0] + "\n時間: " + response[1] + "\n北緯: " + response[2] + "\n東經: " + response[3] + "\n規模: " + response[4] + "\n深度: " + response[5] + "\n位置: " + response[6] + "\n地震報告:\nhttps://www.cwb.gov.tw" + response[7] + "\n輸入 [即時地震] 獲取即時發生的地震")
+    } else if (event.message.text == "關於機器人") {
+        event.reply("DWHS 天氣小幫手\n(DWHS Weather Helper Service)\n\n版本: " + ver + "\n\n由 JavaScript 開發的 Line 機器人，依賴 ExpTech API Service\n\n• 貢獻者\nwhes1015 - 程式開發\n郭彥銘 - 圖像設計\n\n• 遵守 AGPL-3.0 開源協議\n\n• GitHub\nhttps://github.com/ExpTechTW/DWHS\n• userID\n" + event.source.userId)
+    } else if (event.message.text == "即時地震") {
+        let data = {
+            "APIkey": "a5ef9cb2cf9b0c86b6ba71d0fc39e329",
+            "Function": "data",
+            "Type": "earthquake",
+            "FormatVersion": 1
+        }
+        let res = await API.main(ExpTech, data)
+        let response = res.data["response"]
+        await client.push(event.source.userId, {
+            type: 'image',
+            originalContentUrl: response["map"],
+            previewImageUrl: response["map"]
+        })
+        await client.push(event.source.userId, {
+            type: 'image',
+            originalContentUrl: response["data"],
+            previewImageUrl: response["data"]
+        })
+        await client.push(event.source.userId, {
+            type: 'image',
+            originalContentUrl: response["level"],
+            previewImageUrl: response["level"]
+        })
+        await client.push(event.source.userId, "地震發生後最快 2 分鐘內即可獲取此項數據\n上圖時間為 UT 時間 請自行轉換為 UT+8 (TW 台灣)")
     }
-});
+})
 //#endregion
 
 //#region 監聽端口
